6c584f74fa16f30cfa128687c3ceb63a1702b02b
Shlok-21 <shlokshivkar21@gmail.com>
2024-02-25 17:26:06 +0530

    Data transformation completed with pkl file

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,6 +4,9 @@ sys.path.append('D:\\nettech\\StudentsPerformance\\src')
 from src.logger import logging
 from src.exception import CustomException
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 import pandas as pd 
 from sklearn.model_selection import train_test_split
 
@@ -20,23 +23,23 @@ class DataIngestion:
         self.ingestion_config = DataIngestionConfig()
 
     def initiate_data_ingestion(self):
-        logging.info('Entered Data ingestion component :')
+        logging.info('Entered Data ingestion component.')
         try:
-            df = pd.read_csv('D:\\nettech\\StudentsPerformance\\src\\notebooks\\StudentsPerformance.csv')
-            logging.info('Read the dataset as DataFrame successfully')
+            df = pd.read_csv('D:\\nettech\\StudentsPerformance\\src\\notebooks\\stud.csv')
+            logging.info('Read the dataset as DataFrame successfully.')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
             df.to_csv(self.ingestion_config.raw_data_path, index = False, header = True)
 
-            logging.info('Train-test Split initiated')
+            logging.info('Train-test Split initiated.')
             train_set, test_set = train_test_split(df,test_size = 0.2, random_state = 42)
 
             train_set.to_csv(self.ingestion_config.train_data_path, index = False, header = True)
 
             test_set.to_csv(self.ingestion_config.test_data_path, index = False, header = True)
 
-            logging.info('Train-test split complete')
-            logging.info('Ingestion Completed')
+            logging.info('Train-test split complete.')
+            logging.info('Ingestion Completed.')
 
             return(
                 self.ingestion_config.train_data_path,
@@ -48,4 +51,7 @@ class DataIngestion:
 
 if __name__ == '__main__':
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
