1dc0a1f7ffdaddba6b820d471bfbf89520277690
Shlok-21 <shlokshivkar21@gmail.com>
2024-02-26 17:51:27 +0530

    Model ready, pre deployment phase

--- src/utils.py
+++ src/utils.py
@@ -21,7 +21,6 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
-    
 
 def evaluate_model(X_train, y_train, X_test, y_test, models, param):
     try:
@@ -43,6 +42,14 @@ def evaluate_model(X_train, y_train, X_test, y_test, models, param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
-
+    
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_ob:
+            return dill.load(file_ob)
+        
     except Exception as e:
         raise CustomException(e,sys)
